6ea8cec68b2eb364572a86d34e6b69227d36f159
bkindem <bkindem@ufl.edu>
2026-03-23 23:55:03 -0400

    created the class for the Hash table

---
+++ src/HashTable.hpp
@@ -0,0 +1,19 @@
+#include "Word.hpp"
+#include <vector>
+#include <string>
+
+struct HashTable {
+    std::vector<std::vector<Word*>> hash_table;
+
+    HashTable() : hash_table(256, std::vector<Word*>(0)) {}
+    ~HashTable();
+
+    int hash(int POS, int vibe, int mode, int severity);
+    bool insert(Word* data);
+    std::string get(std::string POS, std::string vibe, std::string mode, int severity);
+
+    //these lines help decode the string values in the word object to ints
+    int decodePOS(std::string str);
+    int decodeVibe(std::string str);
+    int decodeMode(std::string str);
+};
