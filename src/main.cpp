1bb0dcd74763c7afba960139f5d701ee1b3f9f83
Kira Luna <kiraluna1@gmail.com>
2026-03-28 14:52:51 -0400

    Milliseconds to microseconds and other small mods

--- src/main.cpp
+++ src/main.cpp
@@ -108,8 +108,8 @@ int main()
     insertData(hash,tree,"../databases/brainrot-db.csv");
     insertData(hash,tree,"../databases/CSmajor-database.csv");
     auto endTime = std::chrono::high_resolution_clock::now();
-    auto durationTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-    std::cout << "(Took " << durationTime.count() << " milliseconds)" << std::endl;
+    auto durationTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    std::cout << "(Took " << durationTime.count() << " microseconds)" << std::endl;
 
     // Text interface
     std::cout << "\nWelcome to the CS Major insult generator!" << std::endl;
@@ -201,9 +201,9 @@ int main()
                 break;
         }
         endTime = std::chrono::high_resolution_clock::now();
-        durationTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+        durationTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
         std::cout << "Hash Table Insult: " << sentence << std::endl;
-        std::cout << "(Took " << durationTime.count() << " milliseconds)" << std::endl;
+        std::cout << "(Took " << durationTime.count() << " microseconds)" << std::endl;
 
         // Creating insult string with RB Tree
         startTime = std::chrono::high_resolution_clock::now();
@@ -225,9 +225,9 @@ int main()
                 break;
         }
         endTime = std::chrono::high_resolution_clock::now();
-        durationTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+        durationTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
         std::cout << "Red Black Tree Insult: " << sentence << std::endl;
-        std::cout << "(Took " << durationTime.count() << " milliseconds)" << std::endl;
+        std::cout << "(Took " << durationTime.count() << " microseconds)" << std::endl;
 
         // Main menu loop
         std::cout << "\nWould you like to insult again, or exit?\n 1. Generate another insult\n 2. Exit" << std::endl;
